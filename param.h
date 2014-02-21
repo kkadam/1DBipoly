9d271a48db99889a785dfddcc93b47d1a150b03e
Kundan Kadam <kkadam1@tigers.lsu.edu>
2014-02-21 15:02:33 -0600

    forgot param.h earlier

---
+++ param.h
@@ -0,0 +1,7 @@
+integer, parameter :: ns = 1001
+integer, parameter :: nc = 101
+
+double precision, parameter:: np1=0.5
+double precision, parameter:: np2=0.5
+double precision, parameter:: mu1=1
+double precision, parameter:: mu2=1
